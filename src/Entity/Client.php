984f11572f1755277d5676f29f5165ac4a0d5a18
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2022-12-03 14:30:58 +0100

    add: Client extends User

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -6,15 +6,6 @@ use App\Repository\ClientRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ClientRepository::class)]
-class Client
+class Client extends User
 {
-    #[ORM\Id]
-    #[ORM\GeneratedValue]
-    #[ORM\Column]
-    private ?int $id = null;
-
-    public function getId(): ?int
-    {
-        return $this->id;
-    }
 }
