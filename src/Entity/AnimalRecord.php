dae2e52e7dc75848312f34201bcca3d05487e4eb
Romain Leroy <romain.leroy@etudiant.univ-reims.fr>
2022-12-05 17:52:37 +0100

    Création de l'entité AnimalRecord

---
+++ src/Entity/AnimalRecord.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnimalRecordRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: AnimalRecordRepository::class)]
+class AnimalRecord
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column]
+    private ?float $weight = null;
+
+    #[ORM\Column]
+    private ?float $height = null;
+
+    #[ORM\Column(length: 1024, nullable: true)]
+    private ?string $otherInfos = null;
+
+    #[ORM\Column(length: 1024, nullable: true)]
+    private ?string $healthInfos = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getWeight(): ?float
+    {
+        return $this->weight;
+    }
+
+    public function setWeight(float $weight): self
+    {
+        $this->weight = $weight;
+
+        return $this;
+    }
+
+    public function getHeight(): ?float
+    {
+        return $this->height;
+    }
+
+    public function setHeight(float $height): self
+    {
+        $this->height = $height;
+
+        return $this;
+    }
+
+    public function getOtherInfos(): ?string
+    {
+        return $this->otherInfos;
+    }
+
+    public function setOtherInfos(?string $otherInfos): self
+    {
+        $this->otherInfos = $otherInfos;
+
+        return $this;
+    }
+
+    public function getHealthInfos(): ?string
+    {
+        return $this->healthInfos;
+    }
+
+    public function setHealthInfos(?string $healthInfos): self
+    {
+        $this->healthInfos = $healthInfos;
+
+        return $this;
+    }
+}
