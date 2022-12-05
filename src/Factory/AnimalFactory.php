2fabe550cfe7f548f79e54c88a124e944894072d
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2022-12-05 19:12:06 +0100

    Add: Ajout dans la forge le champ "race"

--- src/Factory/AnimalFactory.php
+++ src/Factory/AnimalFactory.php
@@ -39,8 +39,8 @@ final class AnimalFactory extends ModelFactory
     protected function getDefaults(): array
     {
         return [
-            // TODO add your default values here (https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories)
             'name' => self::faker()->text(),
+            'race' => self::faker()->text(),
             'birthday' => self::faker()->dateTime(),
             'gender' => self::faker()->text(),
             'isDomestic' => self::faker()->boolean(),
