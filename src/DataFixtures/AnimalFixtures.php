bf3f45d93d0713594119ed96c0f3a705d86fd30d
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2022-12-05 19:13:28 +0100

    Add: Ajout dans le fixture le createMany

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\AnimalFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,9 +10,7 @@ class AnimalFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        AnimalFactory::createMany(25);
         $manager->flush();
     }
 }
