4216ac0e60b58da4398b8890f0a037ce45aca3fc
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2022-12-09 09:14:20 +0100

    Add: Des dépendances de AnimalFixtures.php

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -5,9 +5,10 @@ namespace App\DataFixtures;
 use App\Factory\AnimalFactory;
 use App\Factory\CategoryAnimalFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class AnimalFixtures extends Fixture
+class AnimalFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
