574d69aeb9f947184b1031a4eaed2a09be6a75fd
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2022-12-09 08:41:37 +0100

    Add: Ajout de categorie

--- src/DataFixtures/AnimalFixtures.php
+++ src/DataFixtures/AnimalFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Factory\AnimalFactory;
+use App\Factory\CategoryAnimalFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -10,7 +11,15 @@ class AnimalFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        AnimalFactory::createMany(25);
+        AnimalFactory::createMany(25, function () {
+            return ['CategoryAnimal' => CategoryAnimalFactory::random()];
+        });
         $manager->flush();
+    }
+    public function getDependencies(): array
+    {
+        return [
+            CategoryFixtures::class,
+        ];
     }
 }
