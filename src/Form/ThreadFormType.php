bdf84114376d9ee1fd53acc036c1f1b8004e8581
Udycz Alexis <alexis.udycz@etudiant.univ-reims.fr>
2022-12-13 17:07:59 +0100

    add: Du formulaire pour un thread

---
+++ src/Form/ThreadFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Thread;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ThreadFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lib')
+            ->add('createdAt')
+            ->add('message')
+            ->add('author')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Thread::class,
+        ]);
+    }
+}
