cdd26f91ceeaf05c7e7022ff947ba4a91b6ee373
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2022-12-19 15:06:55 +0100

    add: Element de base pour la page '/me'

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,17 +2,17 @@
 
 namespace App\Controller;
 
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends AbstractController
 {
-    #[Route('/user', name: 'app_user')]
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
+    #[Route('/me', name: 'app_me')]
     public function index(): Response
     {
-        return $this->render('user/index.html.twig', [
-            'controller_name' => 'UserController',
-        ]);
+        return $this->render('user/index.html.twig');
     }
 }
