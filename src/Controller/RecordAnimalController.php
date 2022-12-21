a448ad91d6adff874cd2662a7d4ac0aebf066dd6
VINCENT GUILLEMOT <vincent.guillemot@etudiant.univ-reims.fr>
2022-12-21 15:55:50 +0100

    Ajout de l'index des records

--- src/Controller/RecordAnimalController.php
+++ src/Controller/RecordAnimalController.php
@@ -2,17 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Animal;
+use App\Entity\Client;
+use App\Repository\AnimalRepository;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RecordAnimalController extends AbstractController
 {
-    #[Route('/record/animal', name: 'app_record_animal')]
-    public function index(): Response
+    #[Route('/record/animal/{id}', name: 'app_record_animal')]
+    #[ParamConverter('animal', class: Animal::class)]
+    public function index(Animal $animalUser, AnimalRepository $animalRepository): Response
     {
+        $user = $this->getUser();
+        $isClient = false;
+        if ($user instanceof Client) {
+            $isClient = true;
+            $clientId = $user->getId();
+            $id = $animalUser->getId();
+            $animals = $animalRepository->findById($clientId, $id);
+        }
+
         return $this->render('record_animal/index.html.twig', [
-            'controller_name' => 'RecordAnimalController',
+            'animals' => $animals,
+            'isClient' => $isClient,
         ]);
     }
 }
