bd5a1a72215819430c90681e159956ffc84e326d
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2022-12-26 17:58:54 +0100

    add: De la route index avec les appointments vers le template twig

--- src/Controller/AppointmentsController.php
+++ src/Controller/AppointmentsController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Client;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class AppointmentsController extends AbstractController
 {
+    #[IsGranted('IS_AUTHENTICATED_FULLY')]
     #[Route('/appointments', name: 'app_appointments')]
     public function index(): Response
     {
+        $user = $this->getUser();
+
+        if (!$user instanceof Client) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $appointments = $user->getAppointments();
+
         return $this->render('appointments/index.html.twig', [
-            'controller_name' => 'AppointmentsController',
+            'appointments' => $appointments,
         ]);
     }
 }
