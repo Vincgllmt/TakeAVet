cdfacbbe588e8ba9c37d70d7cf51d940072b805a
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2022-12-28 15:20:09 +0100

    add: WIP des routes /planning/{id}/ et /planning/

--- src/Controller/PlanningController.php
+++ src/Controller/PlanningController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Agenda;
+use App\Repository\VetoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +11,23 @@ use Symfony\Component\Routing\Annotation\Route;
 class PlanningController extends AbstractController
 {
     #[Route('/planning', name: 'app_planning')]
-    public function index(): Response
+    public function index(VetoRepository $vetoRepository): Response
     {
+        $vets = $vetoRepository->findAll();
+        dump($vets);
         return $this->render('planning/index.html.twig', [
-            'controller_name' => 'PlanningController',
+            'vets' => $vets,
+        ]);
+    }
+
+    #[Route('/planning/{id}/',
+        name: 'app_planning_show',
+        requirements: ['id' => "\d+"])]
+    public function show(Agenda $agenda): Response
+    {
+        dump($agenda);
+        return $this->render('planning/show.html.twig', [
+            'agenda' => $agenda,
         ]);
     }
 }
