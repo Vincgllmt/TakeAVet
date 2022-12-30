4532726f2f3f6a998f134386b3ed2f54c066b01f
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2022-12-30 17:40:26 +0100

    add: passage du veto dans le modèle

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,15 +2,25 @@
 
 namespace App\Controller;
 
+use App\Entity\Veto;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[IsGranted('IS_AUTHENTICATED_FULLY')]
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
     public function index(): Response
     {
-        return $this->render('dashboard/index.html.twig');
+        $user = $this->getUser();
+        if (!$user instanceof Veto) {
+            throw $this->createAccessDeniedException();
+        }
+
+        return $this->render('dashboard/index.html.twig', [
+            'veto' => $user,
+        ]);
     }
 }
