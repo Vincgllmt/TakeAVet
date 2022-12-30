42f2f122064a7c344551c011c8915a7b3f640b11
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2022-12-30 18:09:10 +0100

    add: Des informations pour créer un planning

--- src/Form/AgendaFormType.php
+++ src/Form/AgendaFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\Agenda;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,6 +13,19 @@ class AgendaFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+        $builder
+            ->add('timeStart', TimeType::class, [
+                'mapped' => false,
+                'label' => 'Heure de début de votre journée',
+            ])
+            ->add('timeStop', TimeType::class, [
+                'mapped' => false,
+                'label' => 'Heure de fin de votre journée',
+            ])
+            ->add('weekend', CheckboxType::class, [
+                'mapped' => false,
+                'label' => 'Prendre aussi le weekend ?',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
