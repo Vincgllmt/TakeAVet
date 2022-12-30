344730a80cae5d86dd6fe826b1512297bfce8913
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2022-12-30 17:55:49 +0100

    add: D'un create basique pour un planning

--- src/Controller/PlanningController.php
+++ src/Controller/PlanningController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Agenda;
+use App\Entity\Veto;
+use App\Form\AgendaFormType;
+use App\Repository\AgendaRepository;
 use App\Repository\VetoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -15,6 +19,7 @@ class PlanningController extends AbstractController
     {
         $vets = $vetoRepository->findAll();
         dump($vets);
+
         return $this->render('planning/index.html.twig', [
             'vets' => $vets,
         ]);
@@ -26,8 +31,32 @@ class PlanningController extends AbstractController
     public function show(Agenda $agenda): Response
     {
         dump($agenda);
+
         return $this->render('planning/show.html.twig', [
             'agenda' => $agenda,
+        ]);
+    }
+
+    #[Route('/planning/create', name: 'app_planning_create')]
+    public function create(Request $request, AgendaRepository $agendaRepository): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof Veto) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $agendaForm = $this->createForm(AgendaFormType::class);
+        $agendaForm->handleRequest($request);
+
+        if ($agendaForm->isSubmitted() && $agendaForm->isValid()) {
+            $agenda = new Agenda();
+            $agenda->setVeto($user);
+
+            $agendaRepository->save($agenda, true);
+        }
+
+        return $this->renderForm('planning/create.html.twig', [
+            'agenda_form' => $agendaForm,
         ]);
     }
 }
