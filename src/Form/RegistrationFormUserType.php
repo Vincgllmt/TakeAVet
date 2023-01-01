fbd01863f7d131a85d778906060f850dc72189f5
udyc0001 <alexis.udycz@etudiant.univ-reims.fr>
2023-01-01 12:58:35 +0100

    add: css class sur le form 'form_container'

--- src/Form/RegistrationFormUserType.php
+++ src/Form/RegistrationFormUserType.php
@@ -8,6 +8,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\IsTrue;
 use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\NotBlank;
@@ -54,5 +55,14 @@ abstract class RegistrationFormUserType extends AbstractType
                     ]),
                 ],
             ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'attr' => [
+                'class' => 'form_container',
+            ],
+        ]);
     }
 }
