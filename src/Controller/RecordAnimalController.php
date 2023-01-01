04ccfc5f8c4eb6a0405158fd87855656c79d99eb
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-01-01 16:06:29 +0100

    Ajout de la méthode create

--- src/Controller/RecordAnimalController.php
+++ src/Controller/RecordAnimalController.php
@@ -7,17 +7,13 @@ use App\Entity\AnimalRecord;
 use App\Entity\Client;
 use App\Entity\Veto;
 use App\Form\AnimalRecordFormType;
-use App\Form\AnimalType;
 use App\Repository\AnimalRecordRepository;
-use App\Repository\AnimalRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[IsGranted('IS_AUTHENTICATED_FULLY')]
 class RecordAnimalController extends AbstractController
@@ -34,14 +30,15 @@ class RecordAnimalController extends AbstractController
             $clientId = $user->getId();
             $records = $animalRecordRepository->findByAnimal($animalId);
         }
+
         return $this->render('record_animal/index.html.twig', [
             'records' => $records,
             'animal' => $animal,
             'isClient' => $isClient,
         ]);
     }
-    #[Route('/record/{id}/update')]
-    #[ParamConverter('record', class: AnimalRecord::class)]
+
+    #[Route('/record/update/{id}', requirements: ['id' => "\d+"])]
     public function update(AnimalRecord $animalRecord, Request $request, AnimalRecordRepository $recordRepository): Response
     {
         $user = $this->getUser();
@@ -56,56 +53,30 @@ class RecordAnimalController extends AbstractController
             return $this->redirectToRoute('app_animal');
         }
 
-        return $this->renderForm('record/update.twig', [
+        return $this->renderForm('record_animal/update.twig', [
             'record' => $animalRecord,
             'form' => $form,
         ]);
     }
-
-    /**
-     * @see https://symfony.com/doc/5.4/controller/upload_file.html
-     */
     #[Route('/record/create')]
-    public function create(Request $request, AnimalRepository $animalRepository, SluggerInterface $slugger): Response
+    public function create(Request $request, AnimalRecordRepository $animalRecordRepository): Response
     {
         $user = $this->getUser();
         if (!$user instanceof Veto) {
             throw $this->createAccessDeniedException();
         }
-        $animal = new Animal();
-        $form = $this->createForm(AnimalType::class, $animal);
+        $record = new AnimalRecord();
+        $form = $this->createForm(AnimalRecordFormType::class, $record);
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
-            /** @var UploadedFile $photoFile */
-            $photoFile = $form->get('photo')->getData();
-
-            /** @var Animal $handledAnimal * */
-            $handledAnimal = $form->getData();
-
-            if ($photoFile) {
-                $originalFilename = pathinfo($photoFile->getClientOriginalName(), PATHINFO_FILENAME);
-                $safeFilename = $slugger->slug($originalFilename);
-                $newFilename = $safeFilename.'-'.uniqid().'.'.$photoFile->guessExtension();
-
-                try {
-                    $photoFile->move(
-                        $this->getParameter('animal_directory'),
-                        $newFilename
-                    );
-                } catch (FileException $e) {
-                    // ... handle exception if something happens during file upload
-                }
-
-                $handledAnimal->setPhotoPath($newFilename);
-            }
-
-            $handledAnimal->setClientAnimal($user);
-            $animalRepository->save($handledAnimal, true);
+            /** @var AnimalRecord $handledRecord * */
+            $handledRecord = $form->getData();
+            $animalRecordRepository->save($handledRecord, true);
 
             return $this->redirectToRoute('app_animal');
         }
 
-        return $this->renderForm('animal/create.twig', [
+        return $this->renderForm('record_animal/create.twig', [
             'form' => $form,
         ]);
     }
