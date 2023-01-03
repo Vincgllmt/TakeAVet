ab11d6dc7925f9960c1431b281ef8428a8777ccf
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-01-03 18:05:57 +0100

    modification de la méthode index, verifiant l'utilisateur

--- src/Controller/RecordAnimalController.php
+++ src/Controller/RecordAnimalController.php
@@ -23,18 +23,14 @@ class RecordAnimalController extends AbstractController
     public function index(AnimalRecordRepository $animalRecordRepository, Animal $animal): Response
     {
         $user = $this->getUser();
-        $isClient = false;
         $animalId = $animal->getId();
-        if ($user instanceof Client) {
-            $isClient = true;
-            $clientId = $user->getId();
-            $records = $animalRecordRepository->findByAnimal($animalId);
+        if ($user instanceof Client && $animal->getClientAnimal() !== $user) {
+            throw $this->createAccessDeniedException();
         }
-
+        $records = $animalRecordRepository->findByAnimal($animalId);
         return $this->render('record_animal/index.html.twig', [
             'records' => $records,
             'animal' => $animal,
-            'isClient' => $isClient,
         ]);
     }
 
@@ -58,6 +54,7 @@ class RecordAnimalController extends AbstractController
             'form' => $form,
         ]);
     }
+
     #[Route('/record/create', priority: 2)]
     public function create(Request $request, AnimalRecordRepository $animalRecordRepository): Response
     {
