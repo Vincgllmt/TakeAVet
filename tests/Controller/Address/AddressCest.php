262f785e572deb8c0d3d412155c6603b1ab85a1c
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-01-07 23:23:52 +0100

    ajout d'un test qui ajoute une adresse à un client et la modifie

--- tests/Controller/Address/AddressCest.php
+++ tests/Controller/Address/AddressCest.php
@@ -19,4 +19,25 @@ class AddressCest
         $I->see('Mes adresses');
     }
 
+
+    public function testAddressEdit(ControllerTester $I)
+    {
+        $clientProxy = ClientFactory::createOne();
+        $client = $clientProxy->object();
+
+        $I->amLoggedInAs($client);
+        $I->amOnPage('/address');
+        $I->fillField('Nom', 'Maison');
+        $I->fillField('Adresse', '1 rue de la paix');
+        $I->fillField('Code Postal', '75000');
+        $I->fillField('Ville', 'Paris');
+        $I->click('Ajouter');
+        $I->see('Maison');
+
+        $I->click('Changer');
+        $I->fillField('Nom', 'Voisin');
+        $I->click('Modifier');
+        $I->see("Edition de 'Voisin'");
+    }
+
 }
