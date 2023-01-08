1c7da5571339c44485396f897ed8f67bf8c9ac8f
Alexis Udycz <alexis.udycz@etudiant.univ-reims.fr>
2023-01-08 21:06:13 +0100

    add: passage des elements client, animal et de l'actuel appointment dans le template twig.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Veto;
+use App\Repository\AnimalRepository;
 use App\Repository\AppointmentRepository;
+use App\Repository\ClientRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,14 +15,15 @@ use Symfony\Component\Routing\Annotation\Route;
 class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(AppointmentRepository $appointmentRepository): Response
+    public function index(AppointmentRepository $appointmentRepository, AnimalRepository $animalRepository, ClientRepository $clientRepository): Response
     {
         $user = $this->getUser();
         if (!$user instanceof Veto) {
             throw $this->createAccessDeniedException();
         }
 
-        $appointments = $appointmentRepository->findAllOnDate(new \DateTime(), false);
+        $appointments = $appointmentRepository->findAllOnDate($user, new \DateTime(), false);
+        dump($appointments);
         $currentAppointment = null;
         if (count($appointments) > 0) {
             $currentAppointment = $appointments[0];
@@ -29,7 +32,13 @@ class DashboardController extends AbstractController
         return $this->render('dashboard/index.html.twig', [
             'veto' => $user,
             'appointments' => $appointments,
-            'appointment' => $currentAppointment,
+            'current' => $currentAppointment,
+            'current_client' => (null !== $currentAppointment)
+                ? $clientRepository->findOneBy(['id' => $currentAppointment['client_id']])
+                : null,
+            'current_animal' => (null !== $currentAppointment)
+                ? $animalRepository->findOneBy(['id' => $currentAppointment['animal_id']])
+                : null,
         ]);
     }
 }
