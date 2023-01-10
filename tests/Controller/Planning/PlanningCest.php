5ebb6401250b4c8d068d48682a9deefd2ae54fa7
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-01-10 18:35:43 +0100

    ajout d'un test qui vérifie l'ajout d'un planning pour un vétérinaire

--- tests/Controller/Planning/PlanningCest.php
+++ tests/Controller/Planning/PlanningCest.php
@@ -9,7 +9,6 @@ use App\Tests\ControllerTester;
 
 class PlanningCest
 {
-
     public function planningIndex(ControllerTester $I) {
         $I->amOnPage('/planning');
         $I->seeResponseCodeIs(200);
@@ -21,6 +20,26 @@ class PlanningCest
         $veto = $vetoProxy->object();
         $I->amLoggedInAs($veto);
         $I->amOnPage('/planning/' . $vetoProxy->getId());
+    }
+
+    public function planningAdded(ControllerTester $I) {
+        $vetoProxy = VetoFactory::createOne();
+        $veto = $vetoProxy->object();
+        $I->amLoggedInAs($veto);
+        $I->amOnPage('/');
+        $I->click('Mon Dashboard');
+        $I->see('Mon Dashboard');
+        $I->see('Créer mon planning');
+        $I->click('Créer mon planning');
+        $I->see('Créer votre planning');
+        $I->seeCurrentUrlEquals('/planning/create');
+        $I->selectOption('agenda_form[timeStart][hour]', '08');
+        $I->selectOption('agenda_form[timeStart][minute]', '30');
+        $I->selectOption('agenda_form[timeEnd][hour]', '17');
+        $I->selectOption('agenda_form[timeEnd][minute]', '30');
+        $I->click("C'est bon !");
+        $I->amOnPage('planning/');
+        $I->see($veto->getEmail(), 'tr');
     }
 
 }
