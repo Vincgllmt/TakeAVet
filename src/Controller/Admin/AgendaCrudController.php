304bf51c25a6473a7e631404c105387046a8d92d
Vincent Guillemot <vincent.guillemot@etudiant.univ-reims.fr>
2023-01-10 18:54:10 +0100

    Ajout des champs pour AgendaCrudController

--- src/Controller/Admin/AgendaCrudController.php
+++ src/Controller/Admin/AgendaCrudController.php
@@ -3,7 +3,6 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Agenda;
-use App\Entity\Unavailability;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
@@ -14,34 +13,15 @@ class AgendaCrudController extends AbstractCrudController
     {
         return Agenda::class;
     }
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
             IdField::new('id'),
-            AssociationField::new('unavailabilities','unavailabilities')
-                ->setFormType(Unavailability::class)
-                ->formatValue(function (?string $value, Unavailability $entity) {
-                    return $entity->getAgenda()->getUnavailabilities();
-                })->hideOnForm(),
-            /*
-            AssociationField::new('Animal','Animal')
-                ->setFormType(Animal::class)
-                ->formatValue(function (?string $value, AnimalRecord $entity) {
-                    return $entity->getAnimal()?->getName();
-                })->hideOnForm(),
-            AssociationField::new('Animal','Animal')
-                ->setFormType(Animal::class)
-                ->formatValue(function (?string $value, AnimalRecord $entity) {
-                    return $entity->getAnimal()?->getName();
-                })->hideOnForm(),
-            AssociationField::new('Animal','Animal')
-                ->setFormType(Animal::class)
-                ->formatValue(function (?string $value, AnimalRecord $entity) {
-                    return $entity->getAnimal()?->getName();
-                })->hideOnForm()
-
+            AssociationField::new('unavailabilities', 'indisponibiltés'),
+            AssociationField::new('vacations', 'Vacances'),
+            AssociationField::new('days', 'jours'),
+            AssociationField::new('veto', 'Vétérinaire'),
         ];
     }
-    */
 }
