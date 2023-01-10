c7d728fbb1901bea6691c9f770d9f4fa9d1c557e
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-01-10 19:23:20 +0100

    ajout de test sur la route unavailability/id

--- tests/Controller/Unavailability/UnavailabilityCest.php
+++ tests/Controller/Unavailability/UnavailabilityCest.php
@@ -3,7 +3,9 @@
 
 namespace App\Tests\Controller\Unavailability;
 
+use App\Entity\Veto;
 use App\Factory\ClientFactory;
+use App\Factory\VetoFactory;
 use App\Tests\ControllerTester;
 
 class UnavailabilityCest
@@ -16,9 +18,8 @@ class UnavailabilityCest
         $I->amLoggedInAs($client);
         $I->amOnPage('/unavailability/1/delete');
         $I->seeResponseCodeIs(403);
+        $I->amOnPage('/unavailability/1/');
+        $I->seeResponseCodeIs(404);
     }
-
-
-
 
 }
