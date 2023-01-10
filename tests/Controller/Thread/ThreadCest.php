860117b6cf5c295f4f7ebbf88ba4b0462af1251a
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-01-10 19:05:00 +0100

    ajout d'un test qui vérifie qu'un utilisateur connecté peut ajouter une question sans soucis et l'indiquer comme 'résolue' ou pas

--- tests/Controller/Thread/ThreadCest.php
+++ tests/Controller/Thread/ThreadCest.php
@@ -3,7 +3,9 @@
 
 namespace App\Tests\Controller\Thread;
 
+use App\Factory\ClientFactory;
 use App\Tests\ControllerTester;
+use http\Client;
 
 class ThreadCest
 {
@@ -15,7 +17,28 @@ class ThreadCest
         $I->seeCurrentUrlEquals('/login');
     }
 
+    public function testIndexForUser(ControllerTester $I): void
+    {
+        $clientProxy = ClientFactory::createOne();
+        $client = $clientProxy->object();
 
+        $I->amLoggedInAs($client);
+        $I->amOnPage('/threads');
+        $I->seeResponseCodeIs(200);
+        $I->click('Ajouter une question ici');
+        $I->seeCurrentUrlEquals('/thread/create');
+        $I->see('Soumettre une question');
+        $I->fillField('thread_form[lib]', 'Question pour test');
+        $I->click('Soumettre ma question');
+        $I->see('Question pour test');
+        $I->see('Ma question est résolue ?');
+        $I->click('Ma question est résolue ?');
+        $I->see('Question résolue');
+        $I->see('Je veux réouvrir ma question !');
+        $I->click('Je veux réouvrir ma question !');
+        $I->dontSee('Question résolue');
+        $I->seeResponseCodeIs(200);
+    }
 
 
 
