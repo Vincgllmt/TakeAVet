3faf269aecf309a9c96958f5de83250aea2bf32b
Guillemot Vincent <vincent.guillemot@etudiant.univ-reims.fr>
2023-01-10 10:56:23 +0100

    id mis en hideOnForm

--- src/Controller/Admin/CategoryAnimalCrudController.php
+++ src/Controller/Admin/CategoryAnimalCrudController.php
@@ -17,7 +17,8 @@ class CategoryAnimalCrudController extends AbstractCrudController
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+            IdField::new('id')
+                ->hideOnForm(),
             TextField::new('name'),
         ];
     }
