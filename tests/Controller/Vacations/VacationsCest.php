f78fe470ee1de0affdba6b034b45884926dc2a38
perr0112 <clement.perrot@etudiant.univ-reims.fr>
2023-01-10 20:54:01 +0100

    ajout d'un test non fonctionnel -> à corriger

--- tests/Controller/Vacations/VacationsCest.php
+++ tests/Controller/Vacations/VacationsCest.php
@@ -4,6 +4,7 @@
 namespace App\Tests\Controller\Vacations;
 
 use App\Factory\ClientFactory;
+use App\Factory\VetoFactory;
 use App\Tests\ControllerTester;
 
 class VacationsCest
@@ -15,5 +16,27 @@ class VacationsCest
         $I->amOnPage('vacation/2/delete');
         $I->seeResponseCodeIs(403);
     }
+
+    /*
+     * test non fonctionnel, à corriger
+     *
+     * public function vetoCanDeleteVacations(ControllerTester $I) {
+        $vetoProxy = VetoFactory::createOne();
+        $veto = $vetoProxy->object();
+        $I->amLoggedInAs($veto);
+        $I->amOnPage('dashboard');
+        $I->click('Créer mon planning');
+        $I->click("C'est bon !");
+        $I->click("Éditer mon planning");
+        $I->fillField('vacation_form[libVacation]', "Vacances d'été");
+        $I->click('Ajouter');
+        $vetores = $vetoProxy->object();
+        $I->amOnPage('vacation/' . $vetores->getAgenda()->getVacations()[0]->getId() . '/delete');
+        $vetores = $vetoProxy->object();
+        $I->amOnPage('/dashboard');
+        $I->click('Éditer mon planning');
+        $I->see('Edition de votre planning');
+        $I->dontSee('Vacances d\'été');
+    } */
 
 }
